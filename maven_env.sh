1063d76c4960375117d4497fbe33a961f0dd955d
Pavan Surya Prakash Ponnam <psp.ponnam@gmail.com>
2017-11-01 04:36:40 +0530

    Maven Environment Variables

---
+++ maven_env.sh
@@ -0,0 +1,7 @@
+export MAVEN_VERSION=3.5.2
+
+export USER_HOME_DIR="/root"
+
+export SHA=707b1f6e390a65bde4af4cdaf2a24d45fc19a6ded00fff02e91626e3e42ceaff
+
+export BASE_URL=https://apache.osuosl.org/maven/maven-3/${MAVEN_VERSION}/binaries
